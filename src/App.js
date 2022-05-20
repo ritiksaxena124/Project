33b98c706e75ce9ed1dd143b6a4fecccfef0f229
Ritik Saxena <ritiksaxena124@gmail.com>
2022-05-20 15:08:38 +0530

    Courses Section Design

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Wrapper>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
@@ -29,9 +31,12 @@ function App() {
         <Route path="/signup" element={<Signup />} />
 
       </Routes>
+      </Wrapper>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div``
