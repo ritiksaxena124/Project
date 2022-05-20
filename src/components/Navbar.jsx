4dd11ea08853dad51728efb85194d2fb0b2cc5be
Ritik Saxena <ritiksaxena124@gmail.com>
2022-05-20 20:53:09 +0530

    Contactus Page Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ function Navbar() {
 export default Navbar;
 
 const Wrapper = styled.div`
-  padding: 0px 70px;
+  padding: 15px 70px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -61,6 +61,7 @@ const Logo = styled.h2`
   font-size: 30px;
   font-weight: 900;
 `;
+
 const NavMenuWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -75,18 +76,15 @@ const Items = styled.p`
   cursor: pointer;
   padding: 3px 5px;
   margin-right: 15px;
-  padding: 8.5px 10px;
 `;
 
 const NavButtons = styled.div`
   display: flex;
-  margin: 20px 15px;
 `;
 
 const LoginBtn = styled(Button)`
   font-size: 15px !important;
   background: #0c4d4d !important;
-  margin-top: 10px !important;
   padding: 3px 15px !important;
   color: #ffff !important;
   margin-right: 10px !important;
@@ -96,7 +94,6 @@ const LoginBtn = styled(Button)`
 const SignupBtn = styled(Button)`
   font-size: 15px !important;
   background: #9ca3af5e !important;
-  margin-top: 10px !important;
   padding: 3px 15px !important;
   color: black !important;
   margin-right: 10px !important;
